dc439206b7ed3c98f7f6abe087077c5d7559a0cb
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2023-08-30 22:04:51 +0300

    saving data to database

--- src/main/java/com/hoaxify/webService/user/User.java
+++ src/main/java/com/hoaxify/webService/user/User.java
@@ -1,13 +1,23 @@
 package com.hoaxify.webService.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-@Data
+@Data //sınıf için getter, setter, equals, hashCode ve toString gibi temel yöntemleri otomatik olarak oluşturur
+@Entity //Java sınıfının bir JPA varlığı olduğunu belirtir. Bu sayede sınıf, veritabanında bir tabloya karşılık gelecektir.
+@Table(name="users") //Bu anotasyon, veritabanında oluşturulacak tablonun adını belirtir. "users" adında bir tablo oluşturulacak.
 public class User {
 
-	private String username;
+	@Id //Bu anotasyon, sınıfın birincil anahtar (primary key) alanını belirtir. Veritabanında her bir satırı benzersiz şekilde tanımlamak için kullanılır.
+	@GeneratedValue //Bu anotasyon, birincil anahtarın otomatik olarak oluşturulacağını belirtir. Yani, veritabanı otomatik olarak bir değer atayacak.
+	private long id;
 
-	private String displayName;
+	private String userName;
+
+	private String fullName;
 
 	private String password;
 
---
+++ src/main/java/com/hoaxify/webService/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.hoaxify.webService.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
