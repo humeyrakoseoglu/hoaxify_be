7e779fad28d74d6631db852f428c48b082d5755f
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2023-09-03 23:09:37 +0300

    added spring security BCryptPasswordEncoder

--- src/main/java/com/hoaxify/webService/user/UserService.java
+++ src/main/java/com/hoaxify/webService/user/UserService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.webService.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,13 +10,18 @@ public class UserService {
 
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         super();
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void createUser(User user) {
+        String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
         userRepository.save(user);
     }
 
